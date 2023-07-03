4c82fec78d26e72b4820fdddf628a2b04cb3f6e2
miinxxi <whgh13@naver.com>
2023-07-03 15:40:00 +0900

    first

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -11,12 +11,18 @@ const TodoItem = ({ todo, clickRemoveBtnHandler, doneTodoHandler }) => {
             <div className="button-set">
 
                 {/* 삭제버튼 */}
+                {/* className 속성이 'todo-delete-button button'인 button 엘리먼트를 생성합니다. 
+                이 버튼은 "삭제"라는 텍스트를 보여주고, 클릭 이벤트가 발생하면 clickRemoveBtnHandler 함수를 호출하여 해당 todo 항목을 삭제합니다. 
+                todo.id는 각 항목을 구별하는 유일한 값입니다. */}
                 <button
                     className="todo-delete-button button"
                     onClick={() => clickRemoveBtnHandler(todo.id)}
                 >삭제</button>
 
                 {/* 완료, 취소버튼 */}
+                {/* className 속성이 'todo-delete-button button'인 button 엘리먼트를 생성합니다. 
+                이 버튼은 donebutton 변수에 저장된 텍스트를 보여주고, 클릭 이벤트가 발생하면 doneTodoHandler 함수를 호출하여 해당 todo 항목의 완료 상태를 변경합니다. 
+                todo.id는 각 항목을 구별하는 유일한 값입니다. */}
                 <button
                     className="todo-delete-button button"
                     onClick={() => doneTodoHandler(todo.id)}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import TodoItem from "./TodoItem";
+import TodoItem from "./components/TodoItem";
 
+// todos 배열과 clickRemoveBtnHandler, doneTodoHandler라는 props를 받는다.
+// TodoList 컴포넌트를 정의한다. 이 컴포넌트는 todos, clickRemoveBtnHandler, doneTodoHandler라는 세 가지 props를 받는다. 
+// 이 props는 컴포넌트를 사용하는 곳에서 전달되어야 한다.
 const TodoList = ({ todos, clickRemoveBtnHandler, doneTodoHandler }) => {
     return (
         <div className="list-wrapper">
-            {todos.map((todo) => (
+            {/* todos 배열을 순회하면서 각 todo 항목에 대한 TodoItem 컴포넌트를 생성합니다. map 함수는 배열의 각 요소에 대해 주어진 함수를 실행하고, 그 결과를 새로운 배열로 반환합니다. */}
+            {todos.map((todo) => (    
+                // TodoItem 컴포넌트를 생성합니다. 
+                // 이 컴포넌트는 key, todo, clickRemoveBtnHandler, doneTodoHandler라는 props를 받습니다. 
+                // key props는 각 항목을 구별하는 유일한 값으로 사용됩니다. 
+                // todo props는 현재 순회 중인 todo 항목을 전달합니다.  
+                // clickRemoveBtnHandler와 doneTodoHandler는 각각 TodoItem 컴포넌트에서 발생하는 이벤트를 처리하기 위해 전달되는 함수들입니다. 
                 <TodoItem
                     key={`todo-${todo.id}`}
                     todo={todo}
